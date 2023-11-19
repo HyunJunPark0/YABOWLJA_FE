c4e5aba9987539dbd71d5a38e42c148c6cae2e93
oysterjung <resd877@naver.com>
2023-11-19 19:51:45 +0900

    feat: Carousel기능 추가
    배너와 NewProduct에 캐러셀기능 추가.
    swiper라이브러리 사용함.

---
+++ src/components/NewProductCarousel/index.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React, { useRef, useState } from "react";
+
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
+export default function NewProductCarousel() {
+  return (
+    <>
+      <Swiper
+        loop={true}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
